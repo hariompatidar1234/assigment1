8fde51021bdbb01966d894822bf0daa7a65d6644
hariom <hariompatidar32498@gmail.com>
2023-08-09 12:53:10 +0530

    push first time

---
+++ config/routes.rb
@@ -0,0 +1,14 @@
+Rails.application.routes.draw do
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+ root "articles#index"
+
+ # get "/articles", to: "articles#index"
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+  # get "/articles/:id", to: "articles#show"
+  resources :articles do 
+   resources :comments
+  end #replace teo get with resourcces
+
+end
